f4b476efff339e7ab09c775a306d5d94114c9270
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-05-23 15:09:57 -0500

    Simplify things

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Version, Body, Request, Response, Server, Method, StatusCode};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 use std::task::{Poll, Context};
 use tokio::net::{TcpListener, TcpStream};
 use futures_util::{
-    future::TryFutureExt,
-    stream::{Stream, StreamExt, TryStreamExt},
+    stream::*,
 };
 use std::pin::Pin;
-use tokio_rustls::server::TlsStream;
-use tokio_rustls::TlsAcceptor;
+
+#[cfg(feature = "https")]
+use tokio_rustls::{
+    server::TlsStream,
+    TlsAcceptor
+};
+
+#[cfg(feature = "https")]
 use rustls::internal::pemfile;
 
 async fn hello_world(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let mut response = Response::new(Body::empty());
-    *response.version_mut() = Version::HTTP_2;
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
@@ -68,46 +72,51 @@ async fn shutdown_signal() {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // We'll bind to 127.0.0.1:8080
+    async_main().await
+}
+
+async fn async_main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-
-    // Build TLS configuration.
-    let tls_cfg = {
-        // Load public certificate.
-        let certs = load_certs("sample.pem")?;
-        // Load private key.
-        let key = load_private_key("sample.rsa")?;
-        // Do not use client certificate authentication.
-        let mut cfg = rustls::ServerConfig::new(rustls::NoClientAuth::new());
-        // Select a certificate to use.
-        cfg.set_single_cert(certs, key)
-            .map_err(|e| error(format!("{}", e)))?;
-        // Configure ALPN to accept HTTP/2, HTTP/1.1 in that order.
-        cfg.set_protocols(&[b"h2".to_vec(), b"http/1.1".to_vec()]);
-        std::sync::Arc::new(cfg)
-    };
-
     // Create a TCP listener via tokio.
     let mut tcp = TcpListener::bind(&addr).await?;
-    let tls_acceptor = TlsAcceptor::from(tls_cfg);
-    // Prepare a long-running future stream to accept and serve cients.
-    let incoming_tls_stream = tcp
-        .incoming()
-        .map_err(|e| error(format!("Incoming failed: {:?}", e)))
-        .and_then(move |s| {
-            tls_acceptor.accept(s).map_err(|e| {
-                println!("[!] Voluntary server halt due to client-connection error...");
-                // Errors could be handled here, instead of server aborting.
-                // Ok(None)
-                error(format!("TLS Error: {:?}", e))
-            })
-        })
-        .boxed();
 
-    let server = Server::builder(HyperAcceptor {
-        acceptor: incoming_tls_stream,
-    })
-        .serve(make_service_fn(|_conn| async {
+        #[cfg(feature = "https")] let server = {
+            // Build TLS configuration.
+            let tls_cfg = {
+                // Load public certificate.
+                let certs = load_certs("sample.pem")?;
+                // Load private key.
+                let key = load_private_key("sample.rsa")?;
+                // Do not use client certificate authentication.
+                let mut cfg = rustls::ServerConfig::new(rustls::NoClientAuth::new());
+                // Select a certificate to use.
+                cfg.set_single_cert(certs, key).map_err(|e| Box::new(e))?;
+                // Configure ALPN to accept HTTP/2, HTTP/1.1 in that order.
+                cfg.set_protocols(&[b"h2".to_vec(), b"http/1.1".to_vec()]);
+                std::sync::Arc::new(cfg)
+            };
+            let tls_acceptor = TlsAcceptor::from(tls_cfg);
+            let incoming_stream = tcp
+                .incoming()
+                .map_err(|e| panic!(format!("Incoming failed: {:?}", e)))
+                .and_then(move |s| {
+                    tls_acceptor.accept(s)
+                })
+                .boxed();
+            Server::builder(HyperAcceptor {
+                acceptor: Box::pin(incoming_stream),
+            })
+        };
+        #[cfg(not(feature = "https"))]
+        let server = {
+            let incoming_stream = tcp.incoming();
+            Server::builder(HyperAcceptor {
+                acceptor: Box::pin(incoming_stream)
+            })
+        };
+
+    let server = 
+        server.serve(make_service_fn(|_conn| async {
             // service_fn converts our function into a `Service`
             Ok::<_, Infallible>(service_fn(hello_world))
         }));
@@ -126,11 +135,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
 
+#[cfg(not(feature = "https"))]
+struct HyperAcceptor<'a> {
+    acceptor: Pin<Box<dyn Stream<Item = Result<TcpStream, std::io::Error>> + 'a>>,
+}
 
+#[cfg(not(feature = "https"))]
+impl hyper::server::accept::Accept for HyperAcceptor<'_> {
+    type Conn = TcpStream;
+    type Error = std::io::Error;
+
+    fn poll_accept(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
+        Pin::new(&mut self.acceptor).poll_next(cx)
+    }
+}
+
+#[cfg(feature = "https")]
 struct HyperAcceptor<'a> {
     acceptor: Pin<Box<dyn Stream<Item = Result<TlsStream<TcpStream>, std::io::Error>> + 'a>>,
 }
 
+#[cfg(feature = "https")]
 impl hyper::server::accept::Accept for HyperAcceptor<'_> {
     type Conn = TlsStream<TcpStream>;
     type Error = std::io::Error;
@@ -143,33 +171,28 @@ impl hyper::server::accept::Accept for HyperAcceptor<'_> {
     }
 }
 
+#[cfg(feature = "https")]
 // Load public certificate from file.
 fn load_certs(filename: &str) -> std::io::Result<Vec<rustls::Certificate>> {
     // Open certificate file.
-    let certfile = std::fs::File::open(filename)
-        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let certfile = std::fs::File::open(filename).unwrap();
     let mut reader = std::io::BufReader::new(certfile);
 
     // Load and return certificate.
-    pemfile::certs(&mut reader).map_err(|_| error("failed to load certificate".into()))
+    pemfile::certs(&mut reader).map_err(|_| panic!("failed to load certificate"))
 }
 
+#[cfg(feature = "https")]
 // Load private key from file.
 fn load_private_key(filename: &str) -> std::io::Result<rustls::PrivateKey> {
     // Open keyfile.
-    let keyfile = std::fs::File::open(filename)
-        .map_err(|e| error(format!("failed to open {}: {}", filename, e)))?;
+    let keyfile = std::fs::File::open(filename).unwrap();
     let mut reader = std::io::BufReader::new(keyfile);
 
     // Load and return a single private key.
-    let keys = pemfile::rsa_private_keys(&mut reader)
-        .map_err(|_| error("failed to load private key".into()))?;
+    let keys = pemfile::rsa_private_keys(&mut reader).unwrap();
     if keys.len() != 1 {
-        return Err(error("expected a single private key".into()));
+        panic!("expected a single private key");
     }
     Ok(keys[0].clone())
-}
-
-fn error(err: String) -> std::io::Error {
-    std::io::Error::new(std::io::ErrorKind::Other, err)
 }
